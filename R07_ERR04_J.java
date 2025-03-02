bcffcf22510c4dd3e2e370c0ff16fde2130fe21f
Sarah <144383171+mossillus@users.noreply.github.com>
2025-03-01 23:50:59 -0500

    Create R07_ERR04_J.java
    Three of five user selected vulnerabilities.
    Non-compliant code.

---
+++ R07_ERR04_J.java
@@ -0,0 +1,15 @@
+/*******************************************************************************************************
+* Rule 07. Exceptional Behavior (ERR)
+* ERR04-J. DO not complete aabruptly from a finally block
+* Non-compliant Code:
+*******************************************************************************************************/
+class TryFinally {
+  private static boolean doLogic() {
+    try {
+      throw new IllegalStateException();
+    } finally {
+      System.out.println("logic done");
+      return true;
+    }
+  }
+}
