43124e80f0614c0c81b7e431cc4885c5e8393a66
Sarah <144383171+mossillus@users.noreply.github.com>
2025-03-01 23:57:41 -0500

    Update R08_VNA02_J.java
    Compliant code update.

--- R08_VNA02_J.java
+++ R08_VNA02_J.java
@@ -2,7 +2,6 @@
 * Rule 08. Visibility and Atomicity (VNA)
 * VNA02-J. Ensure that compound operations on shared vvariables are atomic
 * Non-compliant Code:
-*********************************************************************************************************/
 final class Flag {
   private boolean flag = true;
  
@@ -11,6 +10,19 @@ final class Flag {
   }
  
   public boolean getFlag() { // Unsafe
+    return flag;
+  }
+}
+* Compliatn Code:
+*********************************************************************************************************/
+final class Flag {
+  private boolean flag = true;
+ 
+  public synchronized void toggle() {
+    flag ^= true; // Same as flag = !flag;
+  }
+ 
+  public synchronized boolean getFlag() {
     return flag;
   }
 }
