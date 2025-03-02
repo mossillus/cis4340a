812a9d50eedc552faa5884ebf2707b9325b23716
Sarah <144383171+mossillus@users.noreply.github.com>
2025-03-02 00:04:20 -0500

    Update R09_LCK08_J.java
    Compliant code update.

--- R09_LCK08_J.java
+++ R09_LCK08_J.java
@@ -2,7 +2,6 @@
 * Rule 09. Locking (LCK)
 * LCK07-J. Ensure actively held locks are released on exceptional conditions
 * Non-compliant code:
-********************************************************************************************************/
 public final class Client {
   private final Lock lock = new ReentrantLock();
  
@@ -24,6 +23,32 @@ public final class Client {
         } catch (IOException x) {
           // Handle exception
         } 
+      }
+    }
+  }
+}
+* Compliant Code:
+********************************************************************************************************/
+public final class Client {
+  private final Lock lock = new ReentrantLock();
+ 
+  public void doSomething(File file) {
+    InputStream in = null;
+    lock.lock();
+    try {
+      in = new FileInputStream(file);
+      // Perform operations on the open file
+    } catch (FileNotFoundException fnf) {
+      // Forward to handler
+    } finally {
+      lock.unlock();
+ 
+      if (in != null) {
+        try {
+          in.close();
+        } catch (IOException e) {
+          // Forward to handler
+        }
       }
     }
   }
