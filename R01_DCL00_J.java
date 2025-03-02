56f7c4a816e6b5b7ab31727ac4cd04b39501480b
Sarah <144383171+mossillus@users.noreply.github.com>
2025-03-01 23:19:42 -0500

    Create R01_DCL00_J.java
    First of five user selected vulnerabilities.
    Very creative of me I know to do the first one in rule one.
    
    Non-compliant code first

---
+++ R01_DCL00_J.java
@@ -0,0 +1,18 @@
+/*********************************************************************************************************
+* Rule 01. Declarations and Initialization (DCL)
+* DCL00-J. ORevent class initialization cycles
+* Non-compliant code:
+********************************************************************************************************/
+public class Cycle {
+  private final int balance;
+  private static final Cycle c = new Cycle();
+  private static final int deposit = (int) (Math.random() * 100); // Random deposit
+ 
+  public Cycle() {
+    balance = deposit - 10; // Subtract processing fee
+  }
+ 
+  public static void main(String[] args) {
+    System.out.println("The account balance is: " + c.balance);
+  }
+}
